5fb28bfdb17b2ab61cbeea3c6b4e9f942fe770ef
Madhu Rao <madhu.rao@adelaide.edu.au>
2013-05-15 14:30:45 +0930

    Make the changes to the name in the js side and listen to it. Its a cool feature of KO.

--- BindingExample/examplebinding.js
+++ BindingExample/examplebinding.js
@@ -1,7 +1,13 @@
 
 //This is my ViewModel
 function MyNameBinding() {
-	self.myName = "Madhu";
+	self.myName = ko.observable("Madhu");
+	
+	//This listens for the name changes (Input element changes the name)
+	self.myName.subscribe(function () {
+		alert("Name Chnaged");
+	});
+
 }
 
 //This is when KO does the bindings to UI.
